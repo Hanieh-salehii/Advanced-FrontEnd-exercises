3318b71af9598282101cda7a8c3d29e9e1bf6149
Hanieh-salehii <h.salehii.1997@gmail.com>
2024-12-01 09:40:01 +0330

    customizing tailwind color and spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      "dark-red": "#4F0806",
+      "light-red": "#780000",
+      "dark-beige": "#BC6C25",
+      "light-beige": "#DDA15E",
+      "light-green": "#606C38",
+      "dark-green": "#ffc82c",
+      "gray-dark": "#283618",
+    },
+    extend: {
+      spacing: {
+        "spacing-0": "0px",
+        "spacing-1": "4px",
+        "spacing-2": "8px",
+        "spacing-3": "16px",
+        "spacing-4": "24px",
+        "spacing-5": "32px",
+      },
+      borderRadius: {
+        "radius-0":"50%",
+        "radius-1": "4px",
+        "radius-2": "8px",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
-}
+};
